738ad8e003745d9013de92055d3741f37f2648b0
Ankita Gupta <85784446+ankitagupta21@users.noreply.github.com>
2025-07-29 14:38:56 +0530

    Create WeatherService.java

---
+++ src/main/java/com/example/demo/service/WeatherService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+@Component
+public class WeatherService{
+  
+  private static final String apiKey="";
+  
+  private static final String API="";
+
+  @Autowired
+  private RestTemplate restTemplate;
+  
+  public WeatherResponse getWeather(String city){
+    String finalAPI = API.replace("CITY",city).replace("API_KEY",apiKey);
+    ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
+    WeatherResponse body = response.getBody();
+    return body;
+  }
+}
+      
